7f2a14cdd8b7fea06e144efa46e5d31812be69d4
qxan <mehmet.enfa@gmail.com>
2023-01-08 15:33:10 +0300

    navbar changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,21 @@ const Navbar = () => {
     <div>
       <div className="flex justify-between items-center w-screen h-[5rem] p-6 bg-slate-800 text-amber-50">
         <div className="w-[40rem] flex justify-center">
-          <Link to="/" className="text-3xl font-semibold underline  hover:text-yellow-300 active:scale-95">Welcome</Link>
+          <Link
+            to="/"
+            className="text-3xl font-semibold underline  hover:text-yellow-300 active:scale-95"
+          >
+            Welcome
+          </Link>
         </div>
         <div className="p-3">
           <div className="flex gap-3 cursor-pointer mr-[10rem]">
-            <ul className="flex gap-5">
-              <li className="flex gap-1 underline hover:text-yellow-300 active:scale-95">
-                <AiOutlineUnorderedList className="mt-1 text-2xl" />
-                <Link to="/mylist" className="text-xl">My List</Link>
-              </li>
-              <li className="flex gap-1  underline hover:text-yellow-300 active:scale-95">
-                <AiOutlinePlus className="mt-1 text-2xl" />
-                <button className="text-xl">Add List</button>
-              </li>
-            </ul>
+            <p className="flex gap-1 underline hover:text-yellow-300 active:scale-95">
+              <AiOutlineUnorderedList className="mt-1 text-2xl" />
+              <Link to="/mylist" className="text-xl">
+                My List
+              </Link>
+            </p>
           </div>
         </div>
       </div>
